97c860123f5fab40d9d221f86c4ab673b5a82720
leonnewton <leonnewton@163.com>
2016-06-08 14:27:45 +0800

    add test app

---
+++ selfmodify/src/com/example/selfmodify/TestAdd.java
@@ -0,0 +1,32 @@
+package com.example.selfmodify;
+
+public class TestAdd {
+	
+	
+	public int add(int a, int b){
+		int c;
+		c = a * b;
+		return c;
+		
+	}
+	
+	
+	public int add_test1(int a, int b){
+		int c;
+		c = a * b;
+		return c;
+		
+	}
+	
+	
+	
+	public int add_test2(int a, int b){
+		int c;
+		c = a * b;
+		return c;
+		
+	}
+	
+	
+
+}
